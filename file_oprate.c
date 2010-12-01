85052026b338ebf301f8ef803923689b34b65b2d
Mao Lijie <unixlikeos@gmail.com>
2010-12-01 15:22:06 +0800

    file_oprate.c 	open(create), write, close, open, read, close

--- file_oprate.c
+++ file_oprate.c
@@ -10,7 +10,9 @@ int main()
 	int fd = 0;
 	ssize_t write_num = 0;
     char *str = "Hello, software weekly";
+	char buf_read[25] = {0};
 	size_t count = strlen(str);
+	ssize_t len_read = 0;
 
 	if ((fd = open("./tmp.txt", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR)) < 0) {
 		printf("open file hello.txt fail, errno=%d\n",fd);
@@ -20,6 +22,12 @@ int main()
 		printf("write string error:%d\n",write_num);
 		return -1;
 	}
+	close(fd);
+
+	fd = open("./tmp.txt", O_RDWR);
+	len_read = read(fd, buf_read,write_num);
+	printf("read string from tmp.txt is: \n %s\n", buf_read);
+	close(fd);
     return 0;
 }
 
