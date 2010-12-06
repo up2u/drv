ff691f11b05466efa9404a8404a57eecde5bc7d1
Mao Lijie <unixlikeos@gmail.com>
2010-12-06 22:38:39 +0800

    link_operation.h 	add #define ins_dlist_head(head, node)

--- min_malloc/link_operation.h
+++ min_malloc/link_operation.h
@@ -1,6 +1,7 @@
 #ifndef _LINK_OPERATION_H_
 #define _LINK_OPERATION_H_
 
+/* for general link list */
 #define offsetof(TYPE, MEMBER) (size_t)&(((TYPE *)0)->MEMBER)
 #define container_of(TYPE, MEMBER, ADDR) (size_t)((size_t)ADDR - offsetof(TYPE,MEMBER))
 
@@ -20,12 +21,19 @@ struct dlink_list
 	(node)->next = (node); \
     (node)->prev = (node)
 
-/* inser to the tail of head */
+/* inser to the tail of list */
 #define ins_dlist_tail(head,node) \
 	(head)->prev->next = (node); \
 	(node)->prev = (head)->prev; \
 	(node)->next = head; \
 	(head)->prev = (node)
+
+/* insert to the head of list */
+#define ins_dlist_head(head, node) \
+	(head)->next->prev = (node); \
+	(node)->next = (head)->next; \
+	(head)->next = (node); \
+	(node)->prev = (head)
 
 #define del_dlist(node) \
 	(node)->prev->next = (node)->next; \
