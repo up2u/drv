ba12153f670e7584b559587a61f6a1f73ffdf0b1
Mao Lijie <unixlikeos@gmail.com>
2010-12-10 09:35:53 +0800

    example.c 	main process and threads,

--- pthread/example.c
+++ pthread/example.c
@@ -10,12 +10,29 @@
 *         
 * Return: 
 *-----------------------------------------------------------------------------*/
-void thread(void)
+void thread1(void)
 {
-	int i = 0;
-	for(i=0; i<3; i++){
-		printf("this is a pthread.\n");	
+	int j = 0;
+	for(j=0; j<3; j++){
+		printf("this is pthread. 1\n");	
 		usleep(1000);
+	}	
+}
+
+/*-----------------------------------------------------------------------------
+* Function: 
+* Purpose: 
+*         
+* Parameters:
+*         
+* Return: 
+*-----------------------------------------------------------------------------*/
+void thread2(void)
+{
+	int k = 0;
+	for(k=0; k<4; k++){
+		printf("this is pthread. 2\n");	
+		usleep(10000);
 	}	
 }
 
@@ -29,19 +46,26 @@ void thread(void)
 *-----------------------------------------------------------------------------*/
 int main()
 {
-	pthread_t id;
+	pthread_t id1;
+	pthread_t id2;
 	int i = 0;
 	int ret = 0;
-	ret = pthread_create(&id, NULL, (void *)thread, NULL);
+	ret = pthread_create(&id1, NULL, (void *)thread1, NULL);
 	if(ret != 0){
 		printf("create pthread error !\n");
 		return -1;
 	}
-	for(i=0; i<3; i++){
-		printf("this is the main process.\n");
-		pthread_join(id, NULL);
-		return 0;	
+	ret = pthread_create(&id2, NULL, (void *)thread2, NULL);
+	if(ret != 0){
+		printf("create pthread error !\n");
+		return -1;
 	}
+	for(i=0; i<2; i++){
+		printf("time %d: this is the main process.\n", i);
+	}
+	pthread_join(id1, NULL);
+	pthread_join(id2, NULL);
+
 	return 0;	
 }
 
