7a41963a9d329d1fa20fb8858aabc07a8ff524e8
Mao Lijie <unixlikeos@gmail.com>
2010-12-13 13:52:46 +0800

    adjust fucntion location.

--- stack/stack.h
+++ stack/stack.h
@@ -1,6 +1,8 @@
 #ifndef _STACH_H_
 #define _STACH_H_
 
+#include <stdbool.h>
+
 typedef long long selemtype;
 
 typedef struct sqstack
@@ -9,5 +11,11 @@ typedef struct sqstack
 	selemtype *top;
 	int stacksize;
 }sqstack;
+
+bool init_stack(sqstack *s);
+bool push(sqstack *s, selemtype e);
+bool pop(sqstack *s, selemtype *e);
+bool print(sqstack *s);
+bool traverse(sqstack *s, bool (* visit)(sqstack *pstack));
 
 #endif
