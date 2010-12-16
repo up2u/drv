22b0ede411e770f32441e401be009a4ce7f04f6f
Mao Lijie <unixlikeos@gmail.com>
2010-12-16 10:43:02 +0800

    bit_not.c 	assigne an decimal, it's some different from assigne like 0x???? 	is uses 2's-complement, so +0 -0 is same.

--- bit/bit_not.c
+++ bit/bit_not.c
@@ -23,11 +23,46 @@ int main()
 	i2 = ~i1;
 	printf("\ti1=%d[%08X],~i1=%d[%08X]\n",i1,i1, i2,i2);
 
-	printf("unsigned int bit not:\n");
+/*assigne a decimal */
+	printf("signed: assigne an decimal\n");
+	i1 = -1;
+	i2 = ~i1;
+	printf("\ti1=%d[%08X],~i1=%d[%08X]\n",i1,i1, i2,i2);
+	i1 = -0;
+	i2 = ~i1;
+	printf("\ti1=%d[%08X],~i1=%d[%08X]\n",i1,i1, i2,i2);
+	i1 = 0;
+	i2 = ~i1;
+	printf("\ti1=%d[%08X],~i1=%d[%08X]\n",i1,i1, i2,i2);
+	i1 = 1;
+	i2 = ~i1;
+	printf("\ti1=%d[%08X],~i1=%d[%08X]\n",i1,i1, i2,i2);
+	
+	printf("\nunsigned int bit not:\n");
 	unsigned int ui1 = 0x00000000;
 	unsigned int ui2 = ~ui1;
 	printf("\tui1=%u[%08X],~ui1=%u[%08X]\n",ui1,ui1, ui2,ui2); /* %u to unsigned int*/
 	ui1 = 0x80000000;
+	ui2 = ~ui1;
+	printf("\tui1=%u[%08X],~ui1=%u[%08X]\n",ui1,ui1, ui2,ui2);
+
+/*assigne a decimal */
+	printf("unsigned: assigne an decimal\n");
+    /*when assign a decimal to an int(signed or unsigned), it will put the 2's-complement
+      of the decimal to the memory of the variable.then check the whether it is signed or
+      not, at last assign the data in memory to variable. example: i=-1, the memory is
+      FFFFFFFF,if 'i' is signed int, output will be -1, if 'i' is unsigned int, output will
+      be 4294967295*/
+	ui1 = -1;
+	ui2 = ~ui1;
+	printf("\tui1=%u[%08X],~ui1=%u[%08X]\n",ui1,ui1, ui2,ui2);
+	ui1 = -0;
+	ui2 = ~ui1;
+	printf("\tui1=%u[%08X],~ui1=%u[%08X]\n",ui1,ui1, ui2,ui2);
+	ui1 = 0;
+	ui2 = ~ui1;
+	printf("\tui1=%u[%08X],~ui1=%u[%08X]\n",ui1,ui1, ui2,ui2);
+	ui1 = 1;
 	ui2 = ~ui1;
 	printf("\tui1=%u[%08X],~ui1=%u[%08X]\n",ui1,ui1, ui2,ui2);
 
