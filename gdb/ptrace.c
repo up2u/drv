c20722304b1c23e70bba415ca3a2cc02c8e88dd2
Mao Lijie <unixlikeos@gmail.com>
2010-12-18 22:22:26 +0800

    ptrace.c 	ptrace, that parent process used to observe or control the     execution of another process . !!  Need to learn more.

---
+++ gdb/ptrace.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+/*for constants ORIG_EAX etc*/  
+#include <sys/reg.h>
+
+int main()
+{
+	pid_t child;
+	long int orig_eax;
+	child = fork();
+	if(child = 0){
+		ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+		execl("/bin/ls", "ls", NULL);
+	}else{
+		wait(NULL);
+		orig_eax = ptrace(PTRACE_PEEKUSER,
+						  child, 4*ORIG_EAX,
+						  NULL);
+		printf("the child made a system call %ld\n",orig_eax);
+		ptrace(PTRACE_CONT,child, NULL, NULL);
+	}
+	return 0;
+}
+
