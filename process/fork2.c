e5b7b272564acc1ba76ef8bf921b5b01359f8ab2
Mao Lijie <unixlikeos@gmail.com>
2011-11-02 09:01:34 +0800

    fork2.c 	when not flush buffer, it may output something many times     use fflush() to flush it

--- process/fork2.c
+++ process/fork2.c
@@ -2,19 +2,26 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+// flush the stream
+
 int main()
 {
 	pid_t p1, p2;
 	while((p1=fork())== -1);
 	if(0==p1){
-		printf("b");// puzzle ? why execute 2 times 
+		printf("b");
+	// the reason that out 2 times 'a' may because of buffer
+	// so use fflush() to flush stream
+		fflush(stdout); 
 	}else{
-		printf("a");// puzzle ? why execute 2 times 	
+		printf("a");
+		fflush(stdout);
 	}
 	
 	while((p2=fork()) == -1);
 	if(0==p2){
 		printf("c");	
+		fflush(stdout);
 	}
 	
 	return 0;
