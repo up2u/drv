faae47eec49b5bbbeb178a013d7666dd595d65c6
Mao Lijie <unixlikeos@gmail.com>
2012-03-17 19:02:03 +0800

    modified:   mymemset.c         > NOTICE : the priority of == and &         > NOTICE : index

--- mymemset.c
+++ mymemset.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
+#define BUF_SIZE 50
+
 void *mymemset(void *s, int c, size_t n)
 {
     char *src = (char *)s;
+    const char chr = (unsigned char)c;
     while(n--){
-        *src++ = c;
+        *src++ = chr;
     }
 }
 
 
 int main(int argc, char *argv[])
 {
-    char buf[100] = {0};
+    char buf[BUF_SIZE] = {0};
     int i = 0;
 
-    mymemset(buf, 'a', 100);
-    while(i++<100){
-        printf("%c", buf[i]);
+    mymemset(buf, '\x52', BUF_SIZE); // oct '\052' = *, hex '\x52' = R
+    while(i++<BUF_SIZE){
+        printf("%c", buf[i-1]); // NOTICE: the i is start from 1
+        fflush(NULL);
         if(0 == (i&7)){ // NOTICE: the priority of == and &
             putchar('\n');
+            fflush(NULL);
         }
     }
     putchar('\n');
+    fflush(NULL);
     return 0;
 }
