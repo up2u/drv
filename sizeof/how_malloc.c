fd121784a2faba5361291a11078f1845eadd83ea
Mao Lijie <unixlikeos@gmail.com>
2012-03-17 16:02:58 +0800

    modified:   how_malloc.c 		> adjust correctly

--- sizeof/how_malloc.c
+++ sizeof/how_malloc.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef int (*pmonth)[31];
+#define num 31
+typedef int (*pmonth)[num];
 
 int main(char argc, char *argv)
 {
     pmonth pm1;
     pmonth pm2;
 
-    pm1 = (pmonth)malloc(31);;
-    pm2 = (int (*)[31])malloc(31);
+    pm1 = (pmonth)malloc(sizeof(int)*num);;
+    pm2 = (int (*)[num])malloc(sizeof(int)*num);
 
 
     printf("sizeof(pm1) is %d\n", sizeof(pm1));
