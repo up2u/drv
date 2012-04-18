62785ed2f54dbebc64acf15f191aef49df1b7490
Mao Lijie <maolijie@andestech.com.cn>
2012-04-18 11:23:15 +0800

    new file:   select.c 		> function select();

---
+++ select.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/select.h>
+
+int main(int argc, char *argv[])
+{
+    fd_set rfds;
+    struct timeval tv;
+    int retval;
+
+    /*watch stdio (fd 0) to see when it has input*/
+    FD_ZERO(&rfds);
+    FD_SET(0,&rfds);
+
+    /*wait for 5 seconds*/
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+
+    retval = select(1, &rfds, NULL, NULL, &tv);
+    /*don't rely on the value of tv now !*/
+
+    if(retval == -1)
+        perror("select()");
+    else if(retval){
+        printf("the retval = %d\n");
+        printf("data is available now. \n");
+    }
+    else
+        printf("NO data within 5 seconds. \n");
+
+    return 0;
+}
