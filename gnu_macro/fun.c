7a80cea8860c2b913040dcb6bc5b5a8926988bc8
Mao Lijie <maolijie@andestech.com>
2012-06-07 12:22:05 +0800

    new file:   fun.c 	new file:   test.c 	> in this instance can't print __FUNCTION__ 	> must use MACRO not fcuntion

---
+++ gnu_macro/fun.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void pf()
+{
+    printf("file is %s, function=%s\n",__FILE__ ,__FUNCTION__);
+}
---
+++ gnu_macro/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("start\n");
+    pf();
+    printf("end\n");
+
+    return 0;
+}
