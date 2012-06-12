74accfa3ad2916db1225fb9b0edf788a417758a0
Mao Lijie <unixlikeos@gmail.com>
2012-06-12 21:58:30 +0800

    modified:   write_ff_fprint.c 	> have difference between 	> 1.log 2.log 	> must do more check !!!

--- file/write_ff_fprint.c
+++ file/write_ff_fprint.c
@@ -3,10 +3,16 @@
 int main()
 {
     unsigned char c = 255;
-    FILE *fd = fopen("./1.log", "w");
+    unsigned char c2; // 1st use unsigned, then use signed char !
+    FILE *fd = fopen("./1.log", "r+");
+    FILE *fd2 = fopen("./2.log", "w");
+    fwrite(&c, 1, 1, fd);
+    fseek(fd, 0, SEEK_SET);
 
-    fprintf(fd, "%02x", c);
+    fread(&c2, 1, 1, fd);
+    fprintf(fd2, "%02x", c2);
 
     fclose(fd);
+    fclose(fd2);
     return 0;
 }
