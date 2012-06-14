1b6e3540fc4e98da571473d7102ac96cb9e9c46b
Mao Lijie <maolijie@andestech.com>
2012-06-14 16:14:18 +0800

    modified:   strtok_strchr_chr.c 	> change the order of strchr and strtok 	> it return right !

--- string/strtok_strchr_chr.c
+++ string/strtok_strchr_chr.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 // use of strtok + strchr !!
-
-// why can't find in strchr  ???
+// bug ? the order of strtok and strchr !
 int main()
 {
     char msg[10] ="a b\n";
+    char *p2 = strchr(msg, '\n');  // strchr before strtok !!
     char *p1 = strtok(msg, " ");
-    char *p2 = strchr(msg, '\n');
 
     printf("1st part is %s\n", p1);
     printf("start addr = %d\n", msg);
-    printf("find addr = %d\n", p2); // why return 0 ???
+    printf("find addr = %d\n", p2); // when strtok used after strchr it return right !!
     printf("offset of is %d\n", p2-msg);
 
     return 0;
