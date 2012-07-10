5dbc5ada1af92198c06da8bd38e19121f6f29ab3
Mao Lijie <maolijie@andestech.com>
2012-07-10 09:38:20 +0800

    modified:   struct_pack.c 	>index the element in union

--- struct/struct_pack.c
+++ struct/struct_pack.c
@@ -15,6 +15,8 @@ int main(int argc, char *argv[])
     struct addr_t t1;
 
     printf("sizeof struct addr_t is %d\n", sizeof(t1));
+    // how to index element in union !
+    printf("sizeof union element %d\n", sizeof(t1.addr.long_addr));
 
     return 0;
 }
