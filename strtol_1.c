5335aa32c434ec2cd706599b2f5b9e7ab6388e4a
Mao Lijie <maolijie@andestech.com>
2012-07-18 22:35:13 +0800

    new file:   strtol_1.c 	> when string out of range for strtol()     > like 0x1ffff ffff, will truncate to 0x1ffff fff ?!

---
+++ strtol_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// NOTE: if string is out of range,
+// then will truncate it ?!
+int main(int argc, char *argv[])
+{
+  // input 0x1ffff ffff  --> will out put 0x1ffff fff
+  int t1 = strtol(argv[1], NULL, 16);
+  printf("it is %x\n", t1);
+
+  return 0;
+}
